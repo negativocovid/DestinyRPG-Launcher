3fa26860b02365baebfa5c221d2d19660be3a4c5
iSoul-Es <marcos-elcapo31@hotmail.com>
2022-11-05 22:22:15 -0300

    login nopremium

--- src/assets/js/panels/login.js
+++ src/assets/js/panels/login.js
@@ -190,7 +190,7 @@ class Login {
         let loginBtn = document.querySelector(".login-btn")
         let mojangBtn = document.querySelector('.mojang')
 
-        mojangBtn.innerHTML = "Offline"
+        mojangBtn.innerHTML = "No Premium"
 
         mojangBtn.addEventListener("click", () => {
             document.querySelector(".login-card").style.display = "none";
